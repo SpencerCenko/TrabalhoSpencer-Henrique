08359be8fb674d72abd11b245dd09509836fbbc7
HeniriqueGnatta <henriquegnatta@gmail.com>
2025-11-19 23:15:02 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,10 +1,20 @@
 import string
 import tkinter as tk #importando a tela
-from tkinter import messagebox, LabelFrame
+from tkinter import messagebox, LabelFrame, PhotoImage
+import database as banco
+import mysql.connector
 
-      
+conexao = mysql.connector.connect(host='localhost',database='login',user='root',password='')
+
+if conexao.is_connected():
+    print("bancooo")
+    cursor = conexao.cursor()
+def logii():
+    print(cursor.execute("select * from log;"))
+    conexao = cursor.fetchall()
 janela = tk.Tk() #definindo o nome da tela
-
+icon = PhotoImage(file="logoPy.png") #definindo uma imagem como icone
+janela.iconphoto(False, icon) #definindo icon como icone da janela
 janela.title("Login")
 janela.geometry('400x300') #definido o tamanho da tela 
 tk.Label(janela, text="Usuário").pack(pady=10) #Onde ira aparecer uma mensagem
@@ -16,14 +26,11 @@ senha.pack(pady=10)
 def log():
     e1 = nome.get()#passando o valor do label para uma varialvel par dar print no nome
     e2 =senha.get()#passando o valor do label para uma varialvel par dar print na senha 
-    if(e1 == 'carro' and e2 == '123'):
+    if(e1 == 'carro' and e2 == '123'): 
         jsecundaria = tk.Tk()#definindo a janela 
-        jsecundaria.title("Principal")#Nome da janela
-        jsecundaria.geometry("400x250")#tamanho da janela
+        jsecundaria.title("Principal")#Nome da janela 
+        jsecundaria.geometry("400x250")#tamanho da janela 
     else:
-        tk.Label(janela,text="nome/senha estao incorretos").pack(padx=10)
-botao = tk.Button(janela, text="Entrar",command=log).pack(padx=10)#botao que executa a ação
-   
-
+        tk.Label(janela,text="nome/senha estao incorretos").pack(padx=10)#mostra quando os dados do usuarios estão incorretos
+botao = tk.Button(janela, text="Entrar",command=logii).pack(padx=10)#botao que executa a ação
 janela.mainloop()
-#achei maneiro esse metodo
