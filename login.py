8400fd5b62fdb97d496dece462528106c0e7b697
HeniriqueGnatta <henriquegnatta@gmail.com>
2025-12-04 14:47:48 +0000

    error

--- login.py
+++ login.py
@@ -1,5 +1,6 @@
 import string
 import tkinter as tk #importando a tela
+<<<<<<< HEAD
 from tkinter import messagebox, LabelFrame, PhotoImage,ttk#são as funções que estou puxando da biblioteca do tkinter
 import mysql.connector
 """
@@ -151,3 +152,32 @@ botao = tk.Button(janela, text="Entrar",command=log).place(height=50,width=100,y
 janela.bind('<Return>',lambda event:log())#Return e no nome do enter no teclado lambda event manda a função pedida
 
 janela.mainloop()
+=======
+from tkinter import messagebox, LabelFrame, PhotoImage
+
+      
+janela = tk.Tk() #definindo o nome da tela
+icon = PhotoImage(file="logoPy.png") #definindo uma imagem como icone
+janela.iconphoto(False, icon) #definindo icon como icone da janela
+janela.title("Login")
+janela.geometry('400x300') #definido o tamanho da tela 
+tk.Label(janela, text="Usuário").pack(pady=10) #Onde ira aparecer uma mensagem
+nome = tk.Entry(janela)# campo de texto onde iremos digitar o nome
+nome.pack(pady=10)
+tk.Label(janela, text="Senha").pack(pady=10)#Onde ira aparecer uma mensagem
+senha = tk.Entry(janela, show="*")# campo de texto onde iremos digitar a senha
+senha.pack(pady=10)
+def log():
+    e1 = nome.get()#passando o valor do label para uma varialvel par dar print no nome
+    e2 =senha.get()#passando o valor do label para uma varialvel par dar print na senha 
+    if(e1 == 'carro' and e2 == '123'):
+        jsecundaria = tk.Tk()#definindo a janela 
+        jsecundaria.title("Principal")#Nome da janela
+        jsecundaria.geometry("400x250")#tamanho da janela
+    else:
+        tk.Label(janela,text="nome/senha estao incorretos").pack(padx=10)
+botao = tk.Button(janela, text="Entrar",command=log).pack(padx=10)#botao que executa a ação
+   
+
+janela.mainloop()
+>>>>>>> 311b418 (error)
